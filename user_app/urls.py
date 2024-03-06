1f3fba4b0f79b36fef455072f21263bb02ff46a1
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2024-03-06 03:24:51 +0300

    added create/update for users and fixed some errors

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path , include
-from .views import RegisterView, LoginApi, UserProfileView, ListUsersView
+from .views import RegisterView, LoginApi, UserProfileView, ListUsersView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginApi.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('users/', ListUsersView.as_view(), name='users'),
+    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -51,3 +51,16 @@ class ListUsersView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
